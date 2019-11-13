65147f68a722d5cd2f99814e4db29e0302f71cf3
ItsNikolor <55848097+ItsNikolor@users.noreply.github.com>
2019-11-13 20:52:57 +0300

    Add files via upload

---
+++ 05/Deserializer.h
@@ -0,0 +1,75 @@
+#pragma once
+#include<iostream>
+#include<sstream>
+#include"Error.h"
+
+class Deserializer {
+	std::istream &in_;
+public:
+	explicit Deserializer(std::istream& in) :in_(in) {}
+
+	template <class T>
+	Error load(T& object)
+	{
+		return object.serialize(*this);
+	}
+
+	template <class... ArgsT>
+	Error operator()(ArgsT&&... args)
+	{
+		return process(args...);
+	}
+
+private:
+	template<class T, class... Args>
+	Error process(T& val, Args&& ... args) {
+		if (process_one(val) == Error::CorruptedArchive) return Error::CorruptedArchive;
+		else return process(std::forward<Args>(args)...);
+	}
+
+	template<class T>
+	Error process(T& val) {
+		return process_one(val);
+	}
+
+	template<class T>
+	Error process_one(T& val) {
+		return Error::CorruptedArchive;
+	}
+
+
+	Error process_one(bool& val) {
+		std::string tmp;
+		in_ >> tmp;
+
+		if (in_.eof()) {
+			in_.clear();
+			return Error::CorruptedArchive;
+		}
+
+		if (tmp[0] == 't') val = true;
+		else if (tmp[0] == 'f') val = false;
+		else return Error::CorruptedArchive;
+
+		return Error::NoError;
+	}
+
+	Error process_one(uint64_t& val) {
+		std::string tmp;
+		in_ >> tmp;
+
+		if (in_.eof()) {
+			in_.clear();
+			return Error::CorruptedArchive;
+		}
+
+		if (tmp[0] == 't' || tmp[0] == 'f') return Error::CorruptedArchive;
+		else {
+			std::stringstream s;
+			s << tmp;
+			s >> val;
+		}
+		return Error::NoError;
+	}
+
+};
---
+++ 05/serializer.h
@@ -0,0 +1,54 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include"Error.h"
+
+class Serializer
+{
+	static constexpr char Separator = ' ';
+	std::ostream& out_;
+public:
+	
+	explicit Serializer(std::ostream &out) :out_(out) {}
+
+	template <class T>
+	Error save(T& object)
+	{
+		return object.serialize(*this);
+	}
+
+	template <class... ArgsT>
+	Error operator()(ArgsT&&... args)
+	{
+		return process(args...);
+	}
+
+private:
+	template<class T, class... Args>
+	Error process(T& val, Args&&... args) {
+		if (process_one(val) == Error::CorruptedArchive) return Error::CorruptedArchive;
+		else return process(std::forward<Args>(args)...);
+	}
+
+	template<class T>
+	Error process(T& val) {
+		return process_one(val);
+	}
+
+	template<class T>
+	Error process_one(T& val) {
+		return Error::CorruptedArchive;
+	}
+
+	Error process_one(bool& val) {
+		if (val) out_ << "t" << Separator;
+		else out_ << "f" << Separator;
+		return Error::NoError;
+	}
+
+	Error process_one(uint64_t& val) {
+		out_ << val << Separator;
+		return Error::NoError;
+	}
+
+};
