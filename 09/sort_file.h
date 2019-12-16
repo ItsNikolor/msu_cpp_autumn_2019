2f822646b894c012f4d1cee9e99b771d757e0a25
ItsNikolor <55848097+ItsNikolor@users.noreply.github.com>
2019-12-16 20:46:10 +0300

    Add files via upload

--- 09/sort_file.h
+++ 09/sort_file.h
@@ -10,25 +10,22 @@
 
 void sort_file(const char* in_path, const char* output_path, const char* tmp_path = "tmp.dat", size_t num_threads = 2) {
 	std::ifstream in_file(in_path, std::ios::binary);
-	if (!in_file) {
-		std::cout << "Can't open input file\n";
-		return;
-	}
+	if (!in_file) 
+		throw(std::exception("Can't open input file\n"));
+	
 
 	std::ofstream out_file(output_path, std::ios::binary);
-	if (!out_file) {
-		std::cout << "Can't open output file\n";
-		return;
-	}
+	if (!out_file) 
+		throw(std::exception("Can't open output file\n"));
+	
 
 	std::ofstream tmp_file(tmp_path, std::ios::binary);
-	if (!tmp_file) {
-		std::cout << "Can't open tmp file\n";
-		return;
-	}
+	if (!tmp_file)
+		throw(std::exception("Can't open tmp file\n"));
+	
 
 	std::mutex read, write;
-	const size_t memory_size = 4194304;// 4 Ð¼Ð±
+	const size_t memory_size = 4194304;// 4 ìá
 	static auto thread_func_lambda = [memory_size](std::mutex& read, std::mutex& write,
 		std::ifstream& in_file, std::ofstream& tmp_file) {
 
@@ -48,8 +45,8 @@ void sort_file(const char* in_path, const char* output_path, const char* tmp_pat
 			}
 	};
 	std::vector<std::thread> threads(num_threads);
-	auto thread_func = bind(thread_func_lambda, ref(read), ref(write), ref(in_file), ref(tmp_file));
-	while (num_threads) threads[--num_threads] = std::move(std::thread(thread_func));
+	auto thread_func = bind(thread_func_lambda, std::ref(read), std::ref(write), std::ref(in_file), std::ref(tmp_file));
+	while (num_threads) threads[--num_threads] = std::thread(thread_func);
 
 	for (auto& t : threads) t.join();
 
@@ -57,12 +54,18 @@ void sort_file(const char* in_path, const char* output_path, const char* tmp_pat
 	in_file.close();
 
 	in_file.open(tmp_path, std::ios::binary);
-	if (!in_file) {
-		std::cout << "Can't open tmp file\n";
-		return;
-	}
+	if (!in_file) 
+		throw(std::exception("Can't open tmp file\n"));
+	
 
-	using T = std::tuple<uint64_t, uint64_t, uint64_t>;
+	struct Triple {
+		uint64_t val, pos, max;
+		Triple(uint64_t val_, uint64_t pos_, uint64_t max_) :val(val_), pos(pos_), max(max_) {};
+		bool operator>(const Triple& A) const {
+			return val > A.val;
+		}
+	};
+	using T = Triple;
 	std::priority_queue<T, std::vector<T>, std::greater<T>> que;
 	size_t i = 0;
 
@@ -73,7 +76,7 @@ void sort_file(const char* in_path, const char* output_path, const char* tmp_pat
 		if (!in_file.read((char*)&count, sizeof(size_t))) break;
 		in_file.read((char*)&tmp, sizeof(uint64_t));
 
-		que.emplace(T{ tmp, in_file.tellg(), (uint64_t)in_file.tellg() + count - sizeof(uint64_t) });
+		que.emplace(T( tmp, in_file.tellg(), (uint64_t)in_file.tellg() + count - sizeof(uint64_t) ));
 		in_file.seekg(count - sizeof(uint64_t), std::ios::cur);
 	}
 	in_file.clear();
@@ -81,9 +84,9 @@ void sort_file(const char* in_path, const char* output_path, const char* tmp_pat
 	while (!que.empty()) {
 
 		auto smallest = que.top();
-		auto smallest_val = std::get<0>(smallest);
-		auto smallest_pos = std::get<1>(smallest);
-		auto smallest_max = std::get<2>(smallest);
+		auto smallest_val = smallest.val;
+		auto smallest_pos = smallest.pos;
+		auto smallest_max = smallest.max;
 		que.pop();
 
 		out_file.write((char*)&smallest_val, sizeof(uint64_t));
@@ -93,6 +96,6 @@ void sort_file(const char* in_path, const char* output_path, const char* tmp_pat
 		uint64_t tmp;
 
 		in_file.read((char*)&tmp, sizeof(uint64_t));
-		que.emplace(T{ tmp,smallest_pos + sizeof(uint64_t),smallest_max });
+		que.emplace(T( tmp,smallest_pos + sizeof(uint64_t),smallest_max ));
 	}
 }
